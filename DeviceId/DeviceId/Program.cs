4520aea0b18ed7f6790c2ee82ab68fc2ba9a159a
Wayne Munro <waynemunro@gmail.com>
2018-02-02 10:07:01 -0800

    Initial commit

---
+++ DeviceId/DeviceId/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+
+
+namespace DeviceId
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "316IoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=/hcfaoeRB5UHm7nDkU/G25TrlTW8V+MGNhFT8JfPzfw=";
+
+
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "316Tunstile01";
+            Device device; try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+            Console.ReadLine();
+        }
+
+    }
+}
