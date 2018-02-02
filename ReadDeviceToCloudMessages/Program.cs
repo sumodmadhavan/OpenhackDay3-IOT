635903a826f6ca156addbce2dbfe91404307ac77
Wayne Munro <waynemunro@gmail.com>
2018-02-02 10:35:32 -0800

    read messages from HUB

---
+++ ReadDeviceToCloudMessages/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ReadDeviceToCloudMessages
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Text;
+    using Microsoft.ServiceBus.Messaging;
+    using System.Threading;
+    using System.Linq;
+
+    public class Program
+    {
+        private const string ConnectionString = "HostName=316IoTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=/hcfaoeRB5UHm7nDkU/G25TrlTW8V+MGNhFT8JfPzfw=";
+        private const string IotHubD2CEndpoint = "messages/events";
+        private static EventHubClient _eventHubClient;
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken cancellationToken)
+        {
+            var eventHubReceiver = _eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested) break;
+                var eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                var data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            _eventHubClient = EventHubClient.CreateFromConnectionString(ConnectionString, IotHubD2CEndpoint);
+            var partitions = _eventHubClient.GetRuntimeInformation().PartitionIds;
+            var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = partitions.Select(partition => ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
